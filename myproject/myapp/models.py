b9c5e1ea480e0fc42713efdcd84433bbf36d70e4
Gustavo Vicentini Deon <gugadeon@gmail.com>
2023-09-22 14:15:11 +0000

    adjusting

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class aaaa(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
 
@@ -19,12 +19,12 @@ class Comments(models.Model):
     content = models.TextField()
     user_id = models.IntegerField()
     post_id = models.ForeignKey(Posts, on_delete=models.CASCADE)
-    response_to = models.ForeignKey(Comments, on_delete=models.CASCADE)
-    created_at = models.DateField(false, true)
+    response_to = models.ForeignKey("self", on_delete=models.CASCADE)
+    created_at = models.DateField("False", "True")
 
 class Interactions(models.Model):
     positive = models.BooleanField()
     user_id = models.IntegerField()
     post_id = models.ForeignKey(Posts, on_delete=models.CASCADE)
     comment_id = models.ForeignKey(Comments, on_delete=models.CASCADE)
-    created_at = models.DateField(false, true)
+    created_at = models.DateField("False", "True")
